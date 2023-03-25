3dcd45259ac777cd02da7f37e180855593202394
raduubraduu <lefter_radu@yahoo.com>
2023-03-25 17:51:15 +0000

    refactored data fetching format and created zone component

---
+++ components/Zone.js
@@ -0,0 +1,10 @@
+function Zone({ zone, tariff }) {
+    return (
+        <div>
+            <h4>{zone}</h4>
+            <div>{tariff}</div>
+        </div>
+    );
+  }
+  
+  export default Zone;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
-import Head from 'next/head'
-import Title from '../components/Title';
-import { getZones } from '../lib/zones';
+import Head from "next/head";
+import Title from "../components/Title";
+import Zone from "../components/Zone";
+import { getZones } from "../lib/zones";
 
 export async function getStaticProps() {
-  console.log('[HomePage] getStaticProps()');
+  console.log("[HomePage] getStaticProps()");
   const zones = await getZones();
-  return { props: { zones },  revalidate: 5* 60,} // seconds 
+  return { props: { zones }, revalidate: 5 * 60 }; // seconds
 }
 
 export default function Home({ zones }) {
-  console.log(zones)
+  console.log(zones);
   return (
     <>
       <Head>
@@ -17,6 +18,7 @@ export default function Home({ zones }) {
       </Head>
       <main className="px-6 py-4">
         <Title>Zone rates</Title>
+        <h2>Search by country</h2>
         {/* <ul>
           {countries.map((country) => (
             <li key={country.id}>
@@ -24,7 +26,26 @@ export default function Home({ zones }) {
             </li>
           ))}
         </ul> */}
+        <h2>Search by zones</h2>
+        <div className="grid grid-cols-2 gap-3">
+          <div>
+            <h3>Landline zones</h3>
+            <div className="grid grid-cols-3 gap-2">
+              {zones.land_zones.map((zone, i) => (
+                <Zone key={i} zone={zone.zone} />
+              ))}
+            </div>
+          </div>
+          <div>
+            <h3>Mobile zones</h3>
+            <div className="grid grid-cols-3 gap-2">
+              {zones.mob_zones.map((zone, i) => (
+                <Zone key={i} zone={zone.zone} />
+              ))}
+            </div>
+          </div>
+        </div>
       </main>
     </>
-  )
+  );
 }
