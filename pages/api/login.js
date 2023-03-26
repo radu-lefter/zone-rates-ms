43916a0b6589d50413327e534b2c2867187f0d01
raduubraduu <lefter_radu@yahoo.com>
2023-03-26 17:27:23 +0100

    updated login api route

---
+++ pages/api/login.js
@@ -0,0 +1,25 @@
+import { fetchJson } from '../../lib/api';
+
+async function handleLogin(req, res) {
+    if (req.method !== 'POST') {
+      res.status(405).end();
+      return;
+    }
+    const { email, password } = req.body;
+    try {
+      const { jwt, user } = await fetchJson('http://127.0.0.1:1337/api/auth/local', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ identifier: email, password }),
+      });
+      //TODO set jwt cookie
+      res.status(200).json({
+        id: user.id,
+        name: user.username,
+      });
+    } catch (err) {
+      res.status(401).end();
+    }
+  }
+  
+  export default handleLogin;
