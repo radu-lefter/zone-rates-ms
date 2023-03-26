7c531cfb58476892a6664682edfb55933d91573b
raduubraduu <lefter_radu@yahoo.com>
2023-03-26 17:30:30 +0100

    implemented cookie usage

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,25 +1,38 @@
-import { fetchJson } from '../../lib/api';
+import { fetchJson } from "../../lib/api";
+import cookie from "cookie";
 
 async function handleLogin(req, res) {
-    if (req.method !== 'POST') {
-      res.status(405).end();
-      return;
-    }
-    const { email, password } = req.body;
-    try {
-      const { jwt, user } = await fetchJson('http://127.0.0.1:1337/api/auth/local', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+  if (req.method !== "POST") {
+    res.status(405).end();
+    return;
+  }
+  const { email, password } = req.body;
+  try {
+    const { jwt, user } = await fetchJson(
+      "http://127.0.0.1:1337/api/auth/local",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ identifier: email, password }),
-      });
-      //TODO set jwt cookie
-      res.status(200).json({
+      }
+    );
+
+    res
+      .status(200)
+      .setHeader(
+        "Set-Cookie",
+        cookie.serialize("jwt", jwt, {
+          path: "/api",
+          httpOnly: true,
+        })
+      )
+      .json({
         id: user.id,
         name: user.username,
       });
-    } catch (err) {
-      res.status(401).end();
-    }
+  } catch (err) {
+    res.status(401).end();
   }
-  
-  export default handleLogin;
+}
+
+export default handleLogin;
